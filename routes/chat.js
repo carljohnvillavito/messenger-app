303d871eace65087e2d8fe1ddb5ef49d7eaae75e
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2025-01-02 13:02:17 +0800

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -1,18 +1,20 @@
 // routes/chat.js
 const express = require('express');
 const router = express.Router();
+const Message = require('../models/message');
 const User = require('../models/user');
 
 function ensureAuthenticated(req, res, next) {
-    if (req.session.userId) {
-        return next();
-    }
-    res.redirect('/login');
+  if (req.session.userId) {
+    return next();
+  }
+  res.redirect('/login');
 }
 
 router.get('/chat', ensureAuthenticated, async (req, res) => {
+    const messages = await Message.find().populate('sender');
     const user = await User.findById(req.session.userId);
-    res.render('chat', { messages: [], user }); // Remove the messages variable, send an empty array.
+    res.render('chat', { messages, user });
 });
 
 module.exports = router;
