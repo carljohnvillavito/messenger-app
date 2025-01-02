cb6f01d312d9db6d17532eb2fca4742f05363c7c
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2025-01-02 16:18:19 +0800

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -16,5 +16,14 @@ router.get('/chat', ensureAuthenticated, async (req, res) => {
     const user = await User.findById(req.session.userId);
     res.render('chat', { messages, user });
 });
+ router.get('/user/:id', ensureAuthenticated, async (req, res) => {
+      const { id } = req.params;
+        const profile = await User.findById(id);
+        if (!profile) {
+          return res.status(404).send("User not found.");
+        }
+            res.render('user_profile', { profile });
+  });
+
 
 module.exports = router;
